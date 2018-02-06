2556801e2dfae7f61313ee93bf62ccd32fa9ee10
Peter Coleman <peter.coleman143@gmail.com>
2018-02-07 08:27:25 +1000

    rectangles

---
+++ rectangles/src/main.rs
@@ -0,0 +1,17 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main () {
+    let rect = Rectangle { width: 30, height: 50 };
+
+    println!(
+        "The area of the rectangle is {} square pxiels",
+        area(&rect)
+    );
+}
+
+fn area (rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
