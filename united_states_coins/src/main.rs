ec4adeddd3f1a1752a654dd8583e6ef4cf587878
Peter Coleman <peter.coleman143@gmail.com>
2018-08-28 08:38:20 +1000

    finished united states coin exersise

--- united_states_coins/src/main.rs
+++ united_states_coins/src/main.rs
@@ -1,5 +1,32 @@
+use std::io;
+
 fn main() {
-    
+    loop {
+        println!("Select a coin to get it's value:");
+
+        println!("1 => Penny");
+        println!("2 => Nickel");
+        println!("3 => Dime");
+        println!("4 => Quarter");
+
+        println!("Enter the corresponding number: ");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+
+        let option = input.chars().next().unwrap();
+
+        let coin = match option {
+            '1' => Coin::Penny,
+            '2' => Coin::Nickel,
+            '3' => Coin::Dime,
+            '4' => Coin::Quarter,
+            _   => break
+        };
+
+        println!("The coin selected is worth: {}\n\n", value_in_cents(coin));
+    }
 }
 
 enum Coin {
